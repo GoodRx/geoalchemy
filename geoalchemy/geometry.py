GoodRx/geoalchemy

commit dd6ae5398fca47cc35b782ca4744b68a9cf62d6c

More Python 3 fixes
Enough to make geoalchemy importable on our favorite project.

geoalchemy/geometry.py
@@ -15,12 +15,12 @@ from geoalchemy.functions import functions
 class Geometry(GeometryBase):
     """Geometry column type. This is the base class for all other
     geometry types like Point, LineString, Polygon, etc.
-    
+
     Converts bind/result values to/from a dialect specific persistent
     geometry value.
-    
+
     """
-    
+
     def result_processor(self, dialect, coltype=None):
         def process(value):
             if value is not None:
@@ -29,15 +29,15 @@ class Geometry(GeometryBase):
                 return value
         return process
 
-# other datatypes can be added as needed, which 
+# other datatypes can be added as needed, which
 # currently only affect DDL statements.
 
 class Point(Geometry):
     name = 'POINT'
-    
+
 class Curve(Geometry):
     name = 'CURVE'
-    
+
 class LineString(Curve):
     name = 'LINESTRING'
 
@@ -57,18 +57,18 @@ class GeometryCollection(Geometry):
     name = 'GEOMETRYCOLLECTION'
 
 class GeometryDDL(object):
-    """A DDL extension which integrates SQLAlchemy table create/drop 
+    """A DDL extension which integrates SQLAlchemy table create/drop
     methods with AddGeometryColumn/DropGeometryColumn functions of
     spatial databases.
-    
+
     Usage::
-    
+
         sometable = Table('sometable', metadata, ...)
-        
+
         GeometryDDL(sometable)
 
         sometable.create()
-    
+
     """
 
     try:
@@ -81,7 +81,7 @@ class GeometryDDL(object):
         # SQLALchemy 0.7
         use_event = True
         columns_attribute = 'columns'
-    
+
     def __init__(self, table):
         if self.use_event:
             self._event.listen(table, 'before_create', self.before_create)
@@ -93,12 +93,12 @@ class GeometryDDL(object):
                       'before-drop', 'after-drop'):
                 table.ddl_listeners[e].append(self)
         self._stack = []
-        
+
     def __call__(self, event, table, bind):
         spatial_dialect = DialectManager.get_spatial_dialect(bind.dialect)
         if event in ('before-create', 'before-drop'):
-            """Remove geometry column from column list (table._columns), so that it 
-            does not show up in the create statement ("create table tab (..)"). 
+            """Remove geometry column from column list (table._columns), so that it
+            does not show up in the create statement ("create table tab (..)").
             Afterwards (on event 'after-create') restore the column list from self._stack.
             """
             regular_cols = [c for c in table.c if not isinstance(c.type, Geometry)]
@@ -106,14 +106,14 @@ class GeometryDDL(object):
             self._stack.append(table.c)
             setattr(table, self.columns_attribute,
                     expression.ColumnCollection(*regular_cols))
-            
+
             if event == 'before-drop':
                 for c in gis_cols:
                     spatial_dialect.handle_ddl_before_drop(bind, table, c)
-                
+
         elif event == 'after-create':
             setattr(table, self.columns_attribute, self._stack.pop())
-            
+
             for c in table.c:
                 if isinstance(c.type, Geometry):
                     spatial_dialect.handle_ddl_after_create(bind, table, c)
@@ -135,14 +135,14 @@ class GeometryDDL(object):
 
 
 class SpatialAttribute(AttributeExtension):
-    """Intercepts 'set' events on a mapped instance attribute and 
+    """Intercepts 'set' events on a mapped instance attribute and
     converts the incoming value to a GIS expression.
-    
+
     """
-    
+
     def set(self, state, value, oldvalue, initiator):
         return _to_gis(value, self.__get_srid(initiator))
- 
+
     def __get_srid(self, initiator):
         """Returns the SRID used for the geometry column that is connected
         to this SpatialAttribute instance."""
@@ -150,14 +150,14 @@ class SpatialAttribute(AttributeExtension):
             return initiator.parent_token.columns[0].type.srid
         except Exception:
             return None
- 
+
 class GeometryExtensionColumn(Column):
     pass
-        
+
 @compiles(GeometryExtensionColumn)
 def compile_column(element, compiler, **kw):
     if isinstance(element.table, (Table, Alias)):
-        if kw.has_key("within_columns_clause") and kw["within_columns_clause"] == True:
+        if "with_columns_clause" in kw and kw["within_columns_clause"] == True:
             if element.type.wkt_internal:
                 if isinstance(compiler.dialect, PGDialect):
                     return compiler.process(functions.wkt(element))
@@ -165,38 +165,38 @@ def compile_column(element, compiler, **kw):
                     "compatible with %s dialect. Defaulting back to WKB"
                     % compiler.dialect.name, exc.SAWarning)
             return compiler.process(functions.wkb(element))
-        
+
     return compiler.visit_column(element, **kw)
-     
-            
+
+
 def GeometryColumn(*args, **kw):
     """Define a declarative column property with GIS behavior.
-    
+
     This just produces orm.column_property() with the appropriate
     extension and comparator_factory arguments.  The given arguments
     are passed through to Column.  The declarative module extracts
     the Column for inclusion in the mapped table.
-    
-    This method can also be used for non-declarative mappings to 
+
+    This method can also be used for non-declarative mappings to
     set the properties for a geometry column when defining the mapping.
-    
+
     """
-    if kw.has_key("comparator"):
+    if "comparator" in kw:
         comparator = kw.pop("comparator")
     else:
         comparator = SpatialComparator
-    
+
     if isinstance(args[0], GeometryExtensionColumn):
         # if used for non-declarative, use the column of the table definition
         column = args[0]
         args = args[1:]
     else:
         # if used for declarative, create a new column
-        column = GeometryExtensionColumn(*args, **kw) 
-    
+        column = GeometryExtensionColumn(*args, **kw)
+
     return column_property(
-        column, 
-        extension=SpatialAttribute(), 
+        column,
+        extension=SpatialAttribute(),
         comparator_factory=comparator
     )
 

geoalchemy/base.py
@@ -7,6 +7,7 @@ except ImportError:
     from sqlalchemy.sql.expression import Function  # sqlalchemy < 0.9
 from sqlalchemy.types import UserDefinedType
 from sqlalchemy.ext.compiler import compiles
+import six
 
 from .utils import from_wkt
 from .functions import functions, _get_function, BaseFunction
@@ -60,7 +61,7 @@ class WKTSpatialElement(SpatialElement, Function):
     """
 
     def __init__(self, desc, srid=4326, geometry_type='GEOMETRY'):
-        assert isinstance(desc, basestring)
+        assert isinstance(desc, six.string_types)
         self.desc = desc
         self.srid = srid
         self.geometry_type = geometry_type
@@ -89,7 +90,6 @@ class WKBSpatialElement(SpatialElement, Function):
     """
 
     def __init__(self, desc, srid=4326, geometry_type='GEOMETRY'):
-        assert isinstance(desc, (basestring, buffer))
         self.desc = desc
         self.srid = srid
         self.geometry_type = geometry_type
@@ -205,7 +205,7 @@ def _to_gis(value, srid_db):
         if isinstance(value.desc, (WKBSpatialElement, WKTSpatialElement)):
             return _check_srid(value.desc, srid_db)
         return _check_srid(value, srid_db)
-    elif isinstance(value, basestring):
+    elif isinstance(value, six.string_types):
         return _check_srid(WKTSpatialElement(value), srid_db)
     elif isinstance(value, expression.ClauseElement):
         return value

geoalchemy/functions.py
@@ -8,6 +8,7 @@ from sqlalchemy import literal
 from sqlalchemy.types import NullType, TypeDecorator
 import types
 import re
+import six
 
 WKT_REGEX = re.compile('.*\\(.*\\).*')
 
@@ -15,10 +16,10 @@ def parse_clause(clause, compiler):
     """This method is used to translate a clause element (geometries, functions, ..).
     According to the type of the clause, a conversion to the database geometry type is added or
     the column clause (column name) or the cascaded clause element is returned.
-        
+
     """
     from geoalchemy.base import SpatialElement, WKTSpatialElement, WKBSpatialElement, DBSpatialElement, GeometryBase
-    
+
     if hasattr(clause, '__clause_element__'):
         # for example a column name
         return clause.__clause_element__()
@@ -29,21 +30,21 @@ def parse_clause(clause, compiler):
         if isinstance(clause, (WKTSpatialElement, WKBSpatialElement)):
             return clause
         if isinstance(clause, DBSpatialElement):
-            return literal(clause.desc, GeometryBase)    
+            return literal(clause.desc, GeometryBase)
         return clause.desc
-    elif isinstance(clause, basestring) and WKT_REGEX.match(clause):
+    elif isinstance(clause, six.string_types) and WKT_REGEX.match(clause):
         return WKTSpatialElement(clause)
-    
-    # for raw parameters    
+
+    # for raw parameters
     return literal(clause)
 
 
 def _get_function(element, compiler, params, within_column_clause):
-    """For elements of type BaseFunction, the database specific function data 
-    is looked up and a executable sqlalchemy.sql.expression.Function object 
+    """For elements of type BaseFunction, the database specific function data
+    is looked up and a executable sqlalchemy.sql.expression.Function object
     is returned.
     """
-    from geoalchemy.dialect import DialectManager 
+    from geoalchemy.dialect import DialectManager
     database_dialect = DialectManager.get_spatial_dialect(compiler.dialect)
     for kls in element.__class__.__mro__:
         try:
@@ -52,17 +53,17 @@ def _get_function(element, compiler, params, within_column_clause):
             continue
     if function_data is None:
         raise Exception("Unsupported function for this dialect")
-    
+
     if isinstance(function_data, list):
         """if we have a list of function names, create cascaded Function objects
         for all function names in the list::
-        
+
             ['TO_CHAR', 'SDO_UTIL.TO_WKTGEOMETRY'] --> TO_CHAR(SDO_UTIL.TO_WKTGEOMETRY(..))
         """
         function = None
         for name in reversed(function_data):
             packages = name.split('.')
-            
+
             if function is None:
                 """for the innermost function use the passed-in parameters as argument,
                 otherwise use the prior created function
@@ -70,14 +71,14 @@ def _get_function(element, compiler, params, within_column_clause):
                 args = params
             else:
                 args = [function]
-                
-            function = Function(packages.pop(-1), 
+
+            function = Function(packages.pop(-1),
                         packagenames=packages,
                         *args
                         )
-        
+
         return function
-    
+
     elif isinstance(function_data, types.FunctionType):
         """if we have a function, call this function with the parameters and return the
         created Function object
@@ -87,106 +88,106 @@ def _get_function(element, compiler, params, within_column_clause):
             flags = element.flags
         else:
             flags = {}
-            
+
         return function_data(params, within_column_clause, **flags)
-    
+
     else:
         packages = function_data.split('.')
-        
-        return Function(packages.pop(-1), 
-                        packagenames=packages, 
+
+        return Function(packages.pop(-1),
+                        packagenames=packages,
                         *params
                         )
 
-    
+
 class BaseFunction(Function):
     """Represents a database function.
-    
+
     When the function is used on a geometry column (r.geom.point_n(2) or Road.geom.point_n(2)),
-    additional arguments are set using __call__. The column or geometry the function is called on 
+    additional arguments are set using __call__. The column or geometry the function is called on
     is stored inside the constructor (see base.SpatialComparator.__getattr__()).
     When the function is called directly (functions.point_n(..., 2)),
     all arguments are set using the constructor.
     """
-    
+
     def __init__(self, *arguments, **kwargs):
         self.arguments = arguments
         self.flags = kwargs.copy()
-        
+
         Function.__init__(self, self.__class__.__name__, **kwargs)
-        
+
     def __call__(self, *arguments, **kwargs):
         if len(arguments) > 0:
             self.arguments =  self.arguments + arguments
-        
+
         if len(kwargs) > 0:
             self.flags.update(kwargs)
-        
+
         return self
 
 class ReturnsGeometryFunction(BaseFunction):
     """Represents a database function which returns a new geometry.
-    
+
     This class adds support for "function chaining", so that a new function
     can called on a function that returns a geometry, for example::
-    
+
         session.scalar(r.road_geom.point_n(5).wkt)
-        
+
     Note that only database generic functions (in package 'functions') can be
     called, but no database specific functions, like::
-    
+
          session.scalar(r.road_geom.point_n(5).sdo_geom_sdo_centroid)
     """
-    
+
     def __getattr__(self, name):
         return getattr(functions, name)(self)
-    
+
     # override the __eq__() operator (allows to use '==' on geometries)
-    def __eq__(self, other): 
+    def __eq__(self, other):
         return functions.equals(self, other)
 
 @compiles(BaseFunction)
 def __compile_base_function(element, compiler, **kw):
-    
+
     params = [parse_clause(argument, compiler) for argument in element.arguments]
-    
-    from geoalchemy.dialect import DialectManager 
+
+    from geoalchemy.dialect import DialectManager
     database_dialect = DialectManager.get_spatial_dialect(compiler.dialect)
-    
+
     if database_dialect.is_member_function(element.__class__):
         geometry = params.pop(0)
         function_name = database_dialect.get_function(element.__class__)
-        
+
         if isinstance(function_name, str):
-            """If the function is defined as String (e.g. "oracle_functions.dims : 'Get_Dims'"), 
-            we construct the function call in the query ourselves. This is because SQLAlchemy, 
-            at this point of the compile process, does not add parenthesis for functions 
+            """If the function is defined as String (e.g. "oracle_functions.dims : 'Get_Dims'"),
+            we construct the function call in the query ourselves. This is because SQLAlchemy,
+            at this point of the compile process, does not add parenthesis for functions
             without arguments when using Oracle.
             Otherwise let SQLAlchemy build the query. Note that this won't work for Oracle with
             functions without parameters."""
-            
+
             return "%s.%s(%s)" % (
                         compiler.process(geometry),
                         function_name,
-                        ", ".join([compiler.process(e) for e in params]) 
+                        ", ".join([compiler.process(e) for e in params])
                         )
         else:
             function = _get_function(element, compiler, params, kw.get('within_columns_clause', False))
-            
+
             return "%s.%s" % (
                         compiler.process(geometry),
-                        compiler.process(function)      
+                        compiler.process(function)
                         )
-            
+
     elif database_dialect.is_property(element.__class__):
         geometry = params.pop(0)
         function_name = database_dialect.get_function(element.__class__)
-        
+
         return "%s.%s" % (
                         compiler.process(geometry),
-                        function_name 
+                        function_name
                         )
-        
+
     else:
         function = _get_function(element, compiler, params, kw.get('within_columns_clause', False))
         return compiler.process(function)
@@ -194,38 +195,38 @@ def __compile_base_function(element, compiler, **kw):
 def BooleanFunction(function, compare_value = 'TRUE'):
     """Wrapper for database function that return 'Boolean-like' values, for example
     SDO_EQUAL returns the string 'TRUE'.
-    
+
     This function adds the necessary comparison to ensure
     that in the WHERE clause the function is compared to `compare_value`
     while in the SELECT clause the function result is returned.
-    
+
     :param function: The function that needs boolean wrapping
     :param compare_value: The value that the function should be compare to
     """
     def function_handler(params, within_column_clause):
         return check_comparison(function(*params), within_column_clause, True, compare_value)
-    
+
     return function_handler
-  
+
 def check_comparison(function, within_column_clause, returns_boolean, compare_value):
     """Because Oracle and MS SQL do not want to know Boolean and functions return 0/1 or the string 'TRUE',
-    we manually have to add a comparison, but only if the function is called inside the where-clause, 
+    we manually have to add a comparison, but only if the function is called inside the where-clause,
     not in the select clause.
-    
+
     For example:
     select .. from .. where SDO_EQUAL(.., ..) = 'TRUE'
     select SDO_EQUAL(.., ..) from ..
-    
+
     """
     if returns_boolean and not within_column_clause:
         return (function == compare_value)
-    else: 
+    else:
         return function
 
 class functions:
     """Functions that implement OGC SFS or SQL/MM and that are supported by most databases
     """
-    
+
     class wkt(BaseFunction):
         """AsText(g)"""
         pass
@@ -234,91 +235,91 @@ class functions:
         """AsBinary(g)"""
         def __init__(self, *arguments):
             BaseFunction.__init__(self, type_=_WKBType, *arguments)
-        
+
     class dimension(BaseFunction):
         """Dimension(g)"""
         pass
-    
+
     class srid(BaseFunction):
         """SRID(g)"""
         pass
-    
+
     class geometry_type(BaseFunction):
         """GeometryType(g)"""
         pass
-    
+
     class is_valid(BaseFunction):
         """IsValid(g)"""
         pass
-    
+
     class is_empty(BaseFunction):
         """IsEmpty(g)"""
         pass
-    
+
     class is_simple(BaseFunction):
         """IsSimple(g)"""
         pass
-    
+
     class is_closed(BaseFunction):
         """IsClosed(g)"""
         pass
-    
+
     class is_ring(BaseFunction):
         """IsRing(g)"""
         pass
-    
+
     class num_points(BaseFunction):
         """NumPoints(g)"""
         pass
-    
+
     class point_n(ReturnsGeometryFunction):
         """PointN(g, n)"""
         pass
-    
+
     class length(BaseFunction):
         """Length(g)"""
         pass
-    
+
     class area(BaseFunction):
         """Area(g)"""
         pass
-    
+
     class x(BaseFunction):
         """X(g)"""
         pass
-    
+
     class y(BaseFunction):
         """Y(g)"""
         pass
-    
+
     class centroid(ReturnsGeometryFunction):
         """Centroid(g)"""
         pass
-    
+
     class boundary(ReturnsGeometryFunction):
         """Boundary"""
         pass
-    
+
     class buffer(ReturnsGeometryFunction):
         """Buffer(g, n)"""
         pass
-    
+
     class convex_hull(ReturnsGeometryFunction):
         """ConvexHull(g)"""
         pass
-    
+
     class envelope(ReturnsGeometryFunction):
         """Envelope(g)"""
         pass
-    
+
     class start_point(ReturnsGeometryFunction):
         """StartPoint(g)"""
         pass
-    
+
     class end_point(ReturnsGeometryFunction):
         """EndPoint(g)"""
         pass
-    
+
     class transform(ReturnsGeometryFunction):
         """Transform(g, srid)"""
         pass
@@ -326,51 +327,51 @@ class functions:
     class equals(BaseFunction):
         """Equals(g1, g2)"""
         pass
-    
+
     class distance(BaseFunction):
         """Distance(g1, g2)"""
         pass
-    
+
     class within_distance(BaseFunction):
         """DWithin(g1, g2, d)"""
         pass
-    
+
     class disjoint(BaseFunction):
         """Disjoint(g1, g2)"""
         pass
-    
+
     class intersects(BaseFunction):
         """Intersects(g1, g2)"""
         pass
-    
+
     class touches(BaseFunction):
         """Touches(g1, g2)"""
         pass
-    
+
     class crosses(BaseFunction):
         """Crosses(g1, g2)"""
         pass
-    
+
     class within(BaseFunction):
         """Within(g1, g2)"""
         pass
-    
+
     class overlaps(BaseFunction):
         """Overlaps(g1, g2)"""
         pass
-    
+
     class gcontains(BaseFunction):
         """Contains(g1, g2)"""
         pass
-    
+
     class covers(BaseFunction):
         """Covers(g1, g2)"""
         pass
-    
+
     class covered_by(BaseFunction):
         """CoveredBy(g1, g2)"""
         pass
-    
+
     class intersection(ReturnsGeometryFunction):
         """Intersection(g1, g2)"""
         pass
@@ -381,7 +382,7 @@ class functions:
            don't support it correctly (PostGIS 1.3), or provide multiple
            implementations (Oracle)."""
         pass
-    
+
     class union(ReturnsGeometryFunction):
         """Union(geometry set)
 
@@ -391,30 +392,30 @@ class functions:
         *Only supported in PostgreSQL/PostGIS currently.*
         """
         pass
-            
+
     class collect(ReturnsGeometryFunction):
         """Collect(geometry set)
-        
+
         Collect is an aggregate function. It is typically used
         in ``SELECT`` clauses.
 
         *Only supported in PostgreSQL/PostGIS currently.*
         """
         pass
-    
+
     class extent(BaseFunction):
         """Extent(geometry set)
-        
+
         Extent is an aggregate function. It is typically used
         in ``SELECT`` clauses.
 
         *Only supported in PostgreSQL/PostGIS currently.*
         """
         pass
-    
+
 @compiles(functions._within_distance)
 def __compile__within_distance(element, compiler, **kw):
-    from geoalchemy.dialect import DialectManager 
+    from geoalchemy.dialect import DialectManager
     database_dialect = DialectManager.get_spatial_dialect(compiler.dialect)
     for kls in element.__class__.__mro__:
         try:
@@ -431,33 +432,33 @@ def __compile__within_distance(element, compiler, **kw):
                  arguments.pop(0), *arguments))
 
 class _WKBType(TypeDecorator):
-    """A helper type which makes sure that the WKB sequence returned from queries like 
+    """A helper type which makes sure that the WKB sequence returned from queries like
     'session.scalar(r.road_geom.wkb)', has the same type as the attribute 'geom_wkb' which
     is filled when querying an object of a mapped class, for example with 'r = session.query(Road).get(1)'::
-    
+
         eq_(session.scalar(self.r.road_geom.wkb), self.r.road_geom.geom_wkb)
-    
-    This type had to be introduced, because for Oracle 'SDO_UTIL.TO_WKBGEOMETRY(..)' returned the type 
+
+    This type had to be introduced, because for Oracle 'SDO_UTIL.TO_WKBGEOMETRY(..)' returned the type
     cx_Oracle.LOB and not a buffer.
-    
-    To modify the behavior of 'process_result_value' for a specific database dialect, overwrite the 
+
+    To modify the behavior of 'process_result_value' for a specific database dialect, overwrite the
     method 'process_wkb' in that dialect.
-    
+
     This class is used inside :class:`functions.wkb`.
-    
+
     """
 
     impl = NullType
 
     def process_result_value(self, value, dialect):
         if value is not None:
-            from geoalchemy.dialect import DialectManager 
+            from geoalchemy.dialect import DialectManager
             database_dialect = DialectManager.get_spatial_dialect(dialect)
-            
+
             return database_dialect.process_wkb(value)
         else:
             return value
 
     def copy(self):
         return _WKBType()
-    
+

setup.py
@@ -27,6 +27,7 @@ setup(name='GeoAlchemy',
       zip_safe=False,
       install_requires=[
           'SQLAlchemy>=0.6.1',
+          'six',
       ],
       entry_points="""
       # -*- Entry points: -*-

commit 51e77862d8aae96e311638d09e2fa36a2f379bf1

Fix a bug introduced in dd6ae5398fca47cc35b782ca4744b68a9cf62d6c
https://github.com/GoodRx/geoalchemy/pull/4/files#r282737830

geoalchemy/geometry.py
@@ -157,7 +157,7 @@ class GeometryExtensionColumn(Column):
 @compiles(GeometryExtensionColumn)
 def compile_column(element, compiler, **kw):
     if isinstance(element.table, (Table, Alias)):
-        if "with_columns_clause" in kw and kw["within_columns_clause"] == True:
+        if kw.get("within_columns_clause") == True:
             if element.type.wkt_internal:
                 if isinstance(compiler.dialect, PGDialect):
                     return compiler.process(functions.wkt(element))